ee033f3b8a18eddca63fda55977f5b79e5109497
Engelbert Niehaus <niehaus@uni-landau.de>
2017-12-04 12:08:56 +0100

    wikidownloader JSCC, JS and MD added

---
+++ libs/wikidownloader.js
@@ -0,0 +1,370 @@
+//#################################################################
+//# Javascript Class: WikiDownloader()
+//#       SuperClass: 
+//#   Class Filename: wikidownloader.js
+//#
+//# Author of Class:      Engelbert Niehaus
+//# email:                niehaus@uni-landau.de
+//# created               18.10.2017
+//# last modifications    2017/11/19 18:10:56
+//# GNU Public License V3 - OpenSource
+//#
+//# created with JavaScript Class Creator JSCC
+//#     https://niebert.github.io/JavascriptClassGenerator
+//#################################################################
+
+//---------------------------------------------------------------------
+//---Store File in Subdirectory /js and import this Class in HTML-File with
+// SCRIPT-Tag:  LANGUAGE="JavaScript" SRC="js/wikidownloader.js"
+//---------------------------------------------------------------------
+//---Constructor of Class WikiDownloader()
+// Call the constructor for creating an instance of class WikiDownloader
+// by the following command in HTML-file that imports this class
+// var vMyInstance = new WikiDownloader();
+//---------------------------------------------------------------------
+//----Attributes-------------------------------------------------------
+//---------------------------------------------------------------------
+// If you want to access the attributes of WikiDownloader, use
+// the attribute name with a leading "this." in the definition of method of WikiDownloader, e.g.
+// this.aName = "Hello World";
+//---------------------------------------------------------------------
+//----Methods----------------------------------------------------------
+//---------------------------------------------------------------------
+// (1) If you want to assign definitions of methods for single instance of the class 'WikiDownloader'
+// they are defined with
+//    this.my_method = function (pPar1,pPar2)
+// this approach allows to overwrite the method definition of single instances dynamically.
+//---------------------------------------------------------------------
+// (2) A prototype definition of methods for 'WikiDownloader' will be set by
+// use the method's name and extend it with 'WikiDownloader'.
+//    WikiDownloader.prototype.my_method = function (pPar1,pPar2)
+// This approach consumes less memory for instances.
+//---------------------------------------------------------------------
+
+	// no superclass defined
+
+
+function WikiDownloader () {
+	// no superclass defined
+
+    //---------------------------------------------------------------------
+    //---Attributes of Class "WikiDownloader()"
+    //---------------------------------------------------------------------
+
+    //---------------------------------------------------------------------
+    //---Methods of Class "WikiDownloader()"
+    //---------------------------------------------------------------------
+	//----PUBLIC Method: WikiDownloader.init(pURL:String)-----
+	// init(pURL)  
+	//	Comment for init
+	//----PUBLIC Method: WikiDownloader.download()-----
+	// download()  
+	//	Comment for download
+	//----PUBLIC Method: WikiDownloader.convertLink():String-----
+	// convertLink()  Return: String
+	//	Comment for convertLink
+	//----PUBLIC Method: WikiDownloader.replaceWikiMath()-----
+	// replaceWikiMath()  
+	//	Comment for replaceWikiMath
+	//----PUBLIC Method: WikiDownloader.setSource(pText:String)-----
+	// setSource(pText)  
+	//	Comment for setSource
+	//----PUBLIC Method: WikiDownloader.getSource():String-----
+	// getSource()  Return: String
+	//	Comment for getSource
+	//----PUBLIC Method: WikiDownloader.convertLink2Web()-----
+	// convertLink2Web()  
+	//	Comment for convertLink2Web
+	//----PUBLIC Method: WikiDownloader.createMediaArray()-----
+	// createMediaArray()  
+	//	Comment for createMediaArray
+	//----PUBLIC Method: WikiDownloader.downloadZIP()-----
+	// downloadZIP()  
+	//	what does downloadZIP do?
+	//----PUBLIC Method: WikiDownloader.convertMedia2Local()-----
+	// convertMedia2Local()  
+	//	Comment for convertMedia2Local
+	
+
+
+}
+//-------------------------------------------------------------------------
+//---END Constructor of Class "WikiDownloader()"
+//-------------------------------------------------------------------------
+
+//
+//#################################################################
+//# PUBLIC Method: init()
+//#    used in Class: WikiDownloader
+//# Parameter:
+//#    pURL:String
+//# Comment:
+//#    Comment for init
+//# 
+//# created with JSCC  2017/03/05 18:13:28
+//# last modifications 2017/11/19 18:10:56
+//#################################################################
+
+WikiDownloader.prototype.init = function (pURL) {
+  //----Debugging------------------------------------------
+  // console.log("js/wikidownloader.js - Call: init(pURL:String)");
+  // alert("js/wikidownloader.js - Call: init(pURL:String)");
+  //----Create Object/Instance of WikiDownloader----
+  //    var vMyInstance = new WikiDownloader();
+  //    vMyInstance.init(pURL);
+  //-------------------------------------------------------
+
+  //----------- INSERT YOUR CODE HERE ---------------
+
+};
+//----End of Method init Definition
+
+
+//#################################################################
+//# PUBLIC Method: download()
+//#    used in Class: WikiDownloader
+//# Parameter:
+//#    
+//# Comment:
+//#    Comment for download
+//# 
+//# created with JSCC  2017/03/05 18:13:28
+//# last modifications 2017/11/19 18:10:56
+//#################################################################
+
+WikiDownloader.prototype.download = function () {
+  //----Debugging------------------------------------------
+  // console.log("js/wikidownloader.js - Call: download()");
+  // alert("js/wikidownloader.js - Call: download()");
+  //----Create Object/Instance of WikiDownloader----
+  //    var vMyInstance = new WikiDownloader();
+  //    vMyInstance.download();
+  //-------------------------------------------------------
+
+  //----------- INSERT YOUR CODE HERE ---------------
+
+};
+//----End of Method download Definition
+
+
+//#################################################################
+//# PUBLIC Method: convertLink()
+//#    used in Class: WikiDownloader
+//# Parameter:
+//#    
+//# Comment:
+//#    Comment for convertLink
+//# Return: String
+//# created with JSCC  2017/03/05 18:13:28
+//# last modifications 2017/11/19 18:10:56
+//#################################################################
+
+WikiDownloader.prototype.convertLink = function () {
+  //----Debugging------------------------------------------
+  // console.log("js/wikidownloader.js - Call: convertLink():String");
+  // alert("js/wikidownloader.js - Call: convertLink():String");
+  //----Create Object/Instance of WikiDownloader----
+  //    var vMyInstance = new WikiDownloader();
+  //    vMyInstance.convertLink();
+  //-------------------------------------------------------
+
+  //----------- INSERT YOUR CODE HERE ---------------
+
+};
+//----End of Method convertLink Definition
+
+
+//#################################################################
+//# PUBLIC Method: replaceWikiMath()
+//#    used in Class: WikiDownloader
+//# Parameter:
+//#    
+//# Comment:
+//#    Comment for replaceWikiMath
+//# 
+//# created with JSCC  2017/03/05 18:13:28
+//# last modifications 2017/11/19 18:10:56
+//#################################################################
+
+WikiDownloader.prototype.replaceWikiMath = function () {
+  //----Debugging------------------------------------------
+  // console.log("js/wikidownloader.js - Call: replaceWikiMath()");
+  // alert("js/wikidownloader.js - Call: replaceWikiMath()");
+  //----Create Object/Instance of WikiDownloader----
+  //    var vMyInstance = new WikiDownloader();
+  //    vMyInstance.replaceWikiMath();
+  //-------------------------------------------------------
+
+  //----------- INSERT YOUR CODE HERE ---------------
+
+};
+//----End of Method replaceWikiMath Definition
+
+
+//#################################################################
+//# PUBLIC Method: setSource()
+//#    used in Class: WikiDownloader
+//# Parameter:
+//#    pText:String
+//# Comment:
+//#    Comment for setSource
+//# 
+//# created with JSCC  2017/03/05 18:13:28
+//# last modifications 2017/11/19 18:10:56
+//#################################################################
+
+WikiDownloader.prototype.setSource = function (pText) {
+  //----Debugging------------------------------------------
+  // console.log("js/wikidownloader.js - Call: setSource(pText:String)");
+  // alert("js/wikidownloader.js - Call: setSource(pText:String)");
+  //----Create Object/Instance of WikiDownloader----
+  //    var vMyInstance = new WikiDownloader();
+  //    vMyInstance.setSource(pText);
+  //-------------------------------------------------------
+
+  //----------- INSERT YOUR CODE HERE ---------------
+
+};
+//----End of Method setSource Definition
+
+
+//#################################################################
+//# PUBLIC Method: getSource()
+//#    used in Class: WikiDownloader
+//# Parameter:
+//#    
+//# Comment:
+//#    Comment for getSource
+//# Return: String
+//# created with JSCC  2017/03/05 18:13:28
+//# last modifications 2017/11/19 18:10:56
+//#################################################################
+
+WikiDownloader.prototype.getSource = function () {
+  //----Debugging------------------------------------------
+  // console.log("js/wikidownloader.js - Call: getSource():String");
+  // alert("js/wikidownloader.js - Call: getSource():String");
+  //----Create Object/Instance of WikiDownloader----
+  //    var vMyInstance = new WikiDownloader();
+  //    vMyInstance.getSource();
+  //-------------------------------------------------------
+
+  //----------- INSERT YOUR CODE HERE ---------------
+
+};
+//----End of Method getSource Definition
+
+
+//#################################################################
+//# PUBLIC Method: convertLink2Web()
+//#    used in Class: WikiDownloader
+//# Parameter:
+//#    
+//# Comment:
+//#    Comment for convertLink2Web
+//# 
+//# created with JSCC  2017/03/05 18:13:28
+//# last modifications 2017/11/19 18:10:56
+//#################################################################
+
+WikiDownloader.prototype.convertLink2Web = function () {
+  //----Debugging------------------------------------------
+  // console.log("js/wikidownloader.js - Call: convertLink2Web()");
+  // alert("js/wikidownloader.js - Call: convertLink2Web()");
+  //----Create Object/Instance of WikiDownloader----
+  //    var vMyInstance = new WikiDownloader();
+  //    vMyInstance.convertLink2Web();
+  //-------------------------------------------------------
+
+  //----------- INSERT YOUR CODE HERE ---------------
+
+};
+//----End of Method convertLink2Web Definition
+
+
+//#################################################################
+//# PUBLIC Method: createMediaArray()
+//#    used in Class: WikiDownloader
+//# Parameter:
+//#    
+//# Comment:
+//#    Comment for createMediaArray
+//# 
+//# created with JSCC  2017/03/05 18:13:28
+//# last modifications 2017/11/19 18:10:56
+//#################################################################
+
+WikiDownloader.prototype.createMediaArray = function () {
+  //----Debugging------------------------------------------
+  // console.log("js/wikidownloader.js - Call: createMediaArray()");
+  // alert("js/wikidownloader.js - Call: createMediaArray()");
+  //----Create Object/Instance of WikiDownloader----
+  //    var vMyInstance = new WikiDownloader();
+  //    vMyInstance.createMediaArray();
+  //-------------------------------------------------------
+
+  //----------- INSERT YOUR CODE HERE ---------------
+
+};
+//----End of Method createMediaArray Definition
+
+
+//#################################################################
+//# PUBLIC Method: downloadZIP()
+//#    used in Class: WikiDownloader
+//# Parameter:
+//#    
+//# Comment:
+//#    what does downloadZIP do?
+//# 
+//# created with JSCC  2017/03/05 18:13:28
+//# last modifications 2017/11/19 18:10:56
+//#################################################################
+
+WikiDownloader.prototype.downloadZIP = function () {
+  //----Debugging------------------------------------------
+  // console.log("js/wikidownloader.js - Call: downloadZIP()");
+  // alert("js/wikidownloader.js - Call: downloadZIP()");
+  //----Create Object/Instance of WikiDownloader----
+  //    var vMyInstance = new WikiDownloader();
+  //    vMyInstance.downloadZIP();
+  //-------------------------------------------------------
+
+  //----------- INSERT YOUR CODE HERE ---------------
+
+};
+//----End of Method downloadZIP Definition
+
+
+//#################################################################
+//# PUBLIC Method: convertMedia2Local()
+//#    used in Class: WikiDownloader
+//# Parameter:
+//#    
+//# Comment:
+//#    Comment for convertMedia2Local
+//# 
+//# created with JSCC  2017/03/05 18:13:28
+//# last modifications 2017/11/19 18:10:56
+//#################################################################
+
+WikiDownloader.prototype.convertMedia2Local = function () {
+  //----Debugging------------------------------------------
+  // console.log("js/wikidownloader.js - Call: convertMedia2Local()");
+  // alert("js/wikidownloader.js - Call: convertMedia2Local()");
+  //----Create Object/Instance of WikiDownloader----
+  //    var vMyInstance = new WikiDownloader();
+  //    vMyInstance.convertMedia2Local();
+  //-------------------------------------------------------
+
+  //----------- INSERT YOUR CODE HERE ---------------
+
+};
+//----End of Method convertMedia2Local Definition
+
+
+    
+//-------------------------------------------
+//---End Definition of Class-----------------
+// JS Class: WikiDownloader
+//-------------------------------------------
---
+++ libs/wikidownloader.md
@@ -0,0 +1,50 @@
+## Javascript Class: WikiDownloader
+created Javascript Class Creator JSCC 2017/11/19 18:09:24
+https://niebert.github.io/JavascriptClassCreator
+File: js/wikidownloader.js
+
+### Attributes: WikiDownloader
+
+### Methods: WikiDownloader
+
+#### init(pURL:String)
+* Visibility: public
+Comment for init) 
+
+#### download()
+* Visibility: public
+Comment for download) 
+
+#### convertLink():String
+* Return Type: :String
+* Visibility: public
+Comment for convertLink):String 
+
+#### replaceWikiMath()
+* Visibility: public
+Comment for replaceWikiMath) 
+
+#### setSource(pText:String)
+* Visibility: public
+Comment for setSource) 
+
+#### getSource():String
+* Return Type: :String
+* Visibility: public
+Comment for getSource):String 
+
+#### convertLink2Web()
+* Visibility: public
+Comment for convertLink2Web) 
+
+#### createMediaArray()
+* Visibility: public
+Comment for createMediaArray) 
+
+#### downloadZIP()
+* Visibility: public
+what does downloadZIP do?) 
+
+#### convertMedia2Local()
+* Visibility: public
+Comment for convertMedia2Local) 
